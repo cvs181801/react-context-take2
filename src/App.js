5aefafda4646b3e7f0c118c92d7a182877425891
cvs181801 <79177146+cvs181801@users.noreply.github.com>
2021-11-09 17:12:14 -0800

    add a handleChange method

--- src/App.js
+++ src/App.js
@@ -5,14 +5,33 @@ import Header from './components/Header'
 import { Usercontextconsumer } from './Usercontext';
 
 class App extends React.Component {
+  state = {
+    newUsername: ""
+  }
+
+  handleChange(event) {
+    const {name, value} = event.target;
+    this.setState({[name]:value})
+
+  }
+
   render() {
       return (
         <div className="App">
           
           <Header/>
+          <input
+                type="text"
+                name="username"
+                placeholder="New username"
+                value={this.state.newUsername}
+                onChange={this.handleChange}
+            />
+            <p>{this.state.newUsername}</p>
+           
           
         </div>
-      );
+      )
   }
 
   
